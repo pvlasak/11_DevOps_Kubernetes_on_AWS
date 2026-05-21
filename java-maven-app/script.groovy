c25420b0cd7a7ac88c4e861046b057c9c70bf2f4
pvlasak <petr.vlasakk@gmail.com>
2026-05-21 22:57:16 +0200

    Jenkinsfile changed to deploy nginx image on EKS cluster. AWS credentials exported as variables

--- java-maven-app/script.groovy
+++ java-maven-app/script.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t petrdeveloper/demo-app:jma-2.1 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
+        sh 'docker push petrdeveloper/demo-app:jma-2.1'
     }
 }
 
